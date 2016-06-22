d5cf7d2452de2b49cbc1f098ffab91a04efecd45
madrus <madrus@gmail.com>
2016-06-22 07:12:38 +0200

    initial commit. working project.

---
+++ index.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    var express = require('express');
+    var app = express();
+    var port = 3700;
+
+    app.set('views', __dirname + '/tpl');
+    app.set('view engine', 'pug');
+    app.engine('pug', require('pug').__express);
+    app.get('/', function (req, res) {
+        res.render('page');
+    });
+
+    app.use(express.static(__dirname + '/public'));
+
+    var io = require('socket.io').listen(app.listen(port));
+    console.log("Listening on port " + port);
+
+    io.sockets.on('connection', function (socket) {
+        socket.emit('message', { messageText: 'welcome to the chat' });
+        socket.on('send', function (data) {
+            io.sockets.emit('message', data);
+        });
+    });
+} ());
+
